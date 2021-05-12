e27d706c834c9fc32c866bac8d8be4636b361754
Eva Antoniuk <39302751+ev-ant@users.noreply.github.com>
2021-05-12 16:16:19 +0200

    Update util.R

--- R/util.R
+++ R/util.R
@@ -6,3 +6,13 @@ pkg_file <- function(..., package = "thesisNord", mustWork = FALSE) {
     file.path(getNamespaceInfo(package, "path"), "inst", ...)
   }
 }
+
+get_base_format = function(format, options = list()) {
+  if (is.character(format)) format = eval(parse(text = format))
+  if (!is.function(format)) stop('The output format must be a function')
+  # make sure named elements in `options` have corresponding named arguments in
+  # the format function, unless the function has the ... argument
+  nms = names(formals(format))
+  if (!('...' %in% nms)) options = options[names(options) %in% c(nms, '')]
+  do.call(format, options)
+}
