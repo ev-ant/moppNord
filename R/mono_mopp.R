9d49de36674bba585f04b1e08422ed3f39547e1f
Eva Antoniuk <39302751+ev-ant@users.noreply.github.com>
2021-05-12 16:19:12 +0200

    Update mono_mopp.R

--- R/mono_mopp.R
+++ R/mono_mopp.R
@@ -314,3 +314,22 @@ highlight_grayscale_latex = function(x) {
 rgb2gray = function(x, maxColorValue = 1) {
   rep(sum(c(.2126, .7152, .0722) * x/maxColorValue), 3)
 }
+
+    pkg_file <- function(..., package = "thesisNord", mustWork = FALSE) {
+  if (is.null(devtools_meta(package))) {
+    system.file(..., package = package, mustWork = mustWork)
+  } else {
+    # used only if package has been loaded with devtools or pkgload
+    file.path(getNamespaceInfo(package, "path"), "inst", ...)
+  }
+}
+
+get_base_format = function(format, options = list()) {
+  if (is.character(format)) format = eval(parse(text = format))
+  if (!is.function(format)) stop('The output format must be a function')
+  # make sure named elements in `options` have corresponding named arguments in
+  # the format function, unless the function has the ... argument
+  nms = names(formals(format))
+  if (!('...' %in% nms)) options = options[names(options) %in% c(nms, '')]
+  do.call(format, options)
+}
